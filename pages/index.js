73e007bef11431a897049c2da345e9e38c06ea5a
Red Chien <redchien@viridiasolutions.com>
2023-10-20 16:59:55 +0800

    feat: Connect to File System and Medium.

--- pages/index.js
+++ pages/index.js
@@ -1,131 +1,105 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+import Alert from "../components/alert";
+import { getSortedPostsData } from "../lib/posts";
+import useSWR from "swr";
+import { format } from "date-fns";
+import Link from "next/link";
+import Date from "../components/date";
 
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+const apiUrl = "https://v1.nocodeapi.com/redchien/medium/nnNgXroWaTVNWxLG";
+// const apiUrl = "https://jsonplaceholder.typicode.com/posts"
 
-      <main>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+const fetcher = async () => {
+    const res = await fetch(apiUrl);
+    const data = await res.json();
+    return data;
+};
 
-        <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
-        </p>
+export default function Home({ allPostsData }) {
+    // console.log("allPostsData :>> ", allPostsData);
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+    const { data, error, isLoading } = useSWR(apiUrl, fetcher);
+    // console.log("data :>> ", data);
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+    return (
+        <Layout home>
+            <Head>
+                <title>{siteTitle}</title>
+            </Head>
+            <section className={utilStyles.headingMd}>
+                {/* <p>
+                    Hi, I'm <b>Red Chien</b>.
+                </p> */}
+                <p>
+                    Practitioner of Web Application development. Self-motivated
+                    and fast learning person with strong teamwork spirit. With
+                    my passionate about research and thinking, I always get a
+                    sense of fulfillment when figuring something out. As a
+                    professional front-end software engineer, I commit myself to
+                    developing practical, scalable, and maintainable solutions.
+                </p>
+            </section>
+            <hr />
+            <section
+                className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}
+            >
+                <h2 className={utilStyles.headingLg}>Posts from File System</h2>
+                {allPostsData.map(({ id, title, date }) => {
+                    return (
+                        <li className={utilStyles.listItem} key={id}>
+                            <Link href={`/posts/${id}`}>{title}</Link>
+                            <br />
+                            <small className={utilStyles.lightText}>
+                                <Date dateString={date} />
+                            </small>
+                        </li>
+                    );
+                })}
+            </section>
+            <hr />
+            {error ? (
+                <Alert type="error">Get Medium Error.</Alert>
+            ) : isLoading ? (
+                <Alert type="success">Loading...</Alert>
+            ) : (
+                <section
+                    className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}
+                >
+                    <h2 className={utilStyles.headingLg}>RSS feed on Medium</h2>
+                    <ul className={utilStyles.list}>
+                        {data.map(
+                            ({ title, author, created, published, link }) => {
+                                return (
+                                    <li
+                                        className={utilStyles.listItem}
+                                        key={created}
+                                    >
+                                        <Link href={link} target="_blank">
+                                            {title}
+                                        </Link>
+                                        <br />
+                                        <small className={utilStyles.lightText}>
+                                            {format(published, "LLLL d, yyyy")}
+                                        </small>
+                                    </li>
+                                );
+                            }
+                        )}
+                    </ul>
+                </section>
+            )}
+        </Layout>
+    );
+}
 
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+export async function getStaticProps() {
+    const allPostsData = getSortedPostsData();
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          text-decoration: none;
-          color: inherit;
-        }
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family:
-            Menlo,
-            Monaco,
-            Lucida Console,
-            Liberation Mono,
-            DejaVu Sans Mono,
-            Bitstream Vera Sans Mono,
-            Courier New,
-            monospace;
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family:
-            -apple-system,
-            BlinkMacSystemFont,
-            Segoe UI,
-            Roboto,
-            Oxygen,
-            Ubuntu,
-            Cantarell,
-            Fira Sans,
-            Droid Sans,
-            Helvetica Neue,
-            sans-serif;
-        }
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
-  );
+    return {
+        props: {
+            allPostsData,
+        },
+    };
 }
